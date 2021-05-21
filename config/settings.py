fa7acbd502f2bb9a7b9f5dc9a6cbcdc8eadffdac
JaechanGo <happypuki@gmail.com>
2021-05-22 06:52:22 +0900

    config_set

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'book',
+    'schedule',
     'widget_tweaks',    #pip install django-widget-tweaks
 ]
 
